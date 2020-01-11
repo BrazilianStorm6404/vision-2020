7189e380caccb7a1c3fe4c4fc764e3c0f709e6e0
BrazilianStorm6404 <36820183+BrazilianStorm6404@users.noreply.github.com>
2020-01-11 10:31:03 -0300

    Criação do arquivo de pré-requisitos do OpenCV

---
+++ opencv_install_requirements.sh
@@ -0,0 +1,23 @@
+sudo apt-get -y install build-essential checkinstall cmake pkg-config yasm
+sudo apt-get -y install git gfortran
+sudo apt-get -y install libjpeg8-dev libjasper-dev libpng12-dev
+ 
+sudo apt-get -y install libtiff5-dev
+ 
+sudo apt-get -y install libtiff-dev
+ 
+sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev
+sudo apt-get -y install libxine2-dev libv4l-dev
+ 
+sudo apt-get -y install libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
+sudo apt-get -y install libgtk2.0-dev libtbb-dev qt5-default
+sudo apt-get -y install libatlas-base-dev
+sudo apt-get -y install libmp3lame-dev libtheora-dev
+sudo apt-get -y install libvorbis-dev libxvidcore-dev libx264-dev
+sudo apt-get -y install libopencore-amrnb-dev libopencore-amrwb-dev
+sudo apt-get -y install libavresample-dev
+sudo apt-get -y install x264 v4l-utils
+ 
+sudo apt-get -y install libprotobuf-dev protobuf-compiler
+sudo apt-get -y install libgoogle-glog-dev libgflags-dev
+sudo apt-get -y install libgphoto2-dev libeigen3-dev libhdf5-dev doxygen
